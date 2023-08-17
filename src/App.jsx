49e4b251ab6fd8059e2d9280523315bee47013c8
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-08-17 19:44:54 +0600

    router add

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import React from 'react';
 import HomePage from './Pages/HomePage';
 import Modal from './Component/Modal';
 import { useModal } from './Context/ModalContext';
+import { Route, Routes } from 'react-router-dom';
+import AdminPage from './Pages/AdminPage';
 
 const App = () => {
   const {isModalOpen, showModal, hideModal } = useModal();
   return (
     <div className='font-body scroll-smooth'>
-      <HomePage/>
+
+      <Routes>
+        <Route path='/' element={ <HomePage/>}/>
+        <Route path='/admin' element={ <AdminPage/>}/>
+      </Routes>
+     
 
 
       <Modal visible={isModalOpen} hide={hideModal}/>
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../dist/output.css'
 import { ModalProvider } from './Context/ModalContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ModalProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ModalProvider>
 
 )
