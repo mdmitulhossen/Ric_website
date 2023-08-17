7734049d19ba9c1602a3d17aff8fb1055244b850
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-08-17 18:58:30 +0600

    ffasf

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import img from "../assets/compressed.gif"
 import '../CSS/hero.css'
 import ReactTyped from 'react-typed';
@@ -8,31 +8,41 @@ const Hero = () => {
     useEffect(() => {
         Aos.init()
     }, [])
+
+    // typing effect
+    // type js plugin
+
+
+
     return (
-        <div  id='hero' className='hero w-full h-[500px] md:h-[650px] relative'>
+        <div id='hero' className='hero w-full h-[500px] md:h-[650px] relative'>
             <div className='w-full h-full bg-black/50'></div>
             <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-4'>
-                <p  data-aos="zoom-in-down" data-aos-duration="3000" className='font-medium md:text-4xl text-2xl'>Welcome to the</p>
-                <h2  data-aos="zoom-in-down" data-aos-duration="3000" className='md:text-[40px] text-4xl font-bold'>FEC</h2>
-                {/* <h1 className='md:text-[50px] text-5xl font-bold'><span className='bg-gradient-to-r from-r1 to-r2 text-transparent bg-clip-text'>Research</span> & Enovation Center</h1> */}
-                <h1  data-aos="zoom-in-down" data-aos-duration="3000" className='md:text-[50px] text-5xl font-bold'>
-                    Research And <span> </span> 
+                <p
+                    // ref={typewriterRef} // Set the ref for the typewriter element
+                    data-aos='zoom-in-down'
+                    data-aos-duration='3000'
+                    className='font-medium md:text-4xl text-2xl '
+                > Welcome to the</p>
+                <h2 data-aos="zoom-in-down" data-aos-duration="3000" className='md:text-[40px] text-4xl font-bold'>FEC</h2>
+
+
+                <h1 data-aos="zoom-in-down" data-aos-duration="3000" className='md:text-[50px] text-5xl font-bold'>
+                    Research And <span> </span>
+
                     <span className='bg-gradient-to-r from-r1 to-r2 text-transparent bg-clip-text'>
                         <ReactTyped
-                            strings={[' Innovation Centre']}
-                            typeSpeed={100}
-                            backSpeed={80}
-                            smartBackspace
-                            shuffle={false}
-                            backDelay={2}
-                            fadeOut={false}
-                            fadeOutDelay={100}
-
+                            strings={[
+                                'Innovation Centre',
+                               ]}
+                            typeSpeed={40}
+                            backSpeed={50}
                             loop >
-
-                        </ReactTyped >
+                        </ReactTyped>
                     </span>
                 </h1>
+
+                <h1 class="typeJsText" data-typetext="I can hack Nasa! Ask me how?"></h1>
 
                 <button className='RICbtn'>Become A Member</button>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,30 @@ export default {
         'r2': '#16AFB2',
         'bg': "#00233F",
       },
+
+        keyframes: {
+          typing: {
+            "0%": {
+              width: "0%",
+              visibility: "hidden"
+            },
+            "100%": {
+              width: "100%"
+            }  
+          },
+          blink: {
+            "50%": {
+              borderColor: "transparent"
+            },
+            "100%": {
+              borderColor: "white"
+            }  
+          }
+        },
+        animation: {
+          typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+        }
+      
     },
   },
   plugins: [],
